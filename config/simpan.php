ee0eac3b6c7dc89b0f28ab69225972dbe7435971
AsaPratama <asapratama515@gmail.com>
2021-09-28 11:45:16 +0800

    Perbaikan ui page tambah ruang dan revisi file config/simpan.php

--- config/simpan.php
+++ config/simpan.php
@@ -8,9 +8,8 @@ if(isset($_POST['simpan'])) {
 	$namaRuangan=$_POST['namaRuangan'];
 	
 	
-	$query = "INSERT INTO truangan VALUES
-						( '', '$namaRuangan', ''  )
-						";
+	$query = "INSERT INTO truangan (idRuangan, namaRuangan, isDeleted)VALUES(NULL,'$namaRuangan',0)";
+					
 	echo"
 	<script>
           alert('Data berhasil di Simpan');
